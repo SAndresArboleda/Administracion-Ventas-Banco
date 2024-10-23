59c901786f72c863bad770eee53676ab75e3477a
SAndresArboleda <simondres@yahoo.es>
2024-10-23 13:29:37 -0500

    ok2

--- Frontend/src/components/Usuario/product/SeguiProduct.jsx
+++ Frontend/src/components/Usuario/product/SeguiProduct.jsx
@@ -22,7 +22,7 @@ export const SeguiProduct = () => {
                 <div style={{ width: '5%' }}>Id</div>
                 <div style={{ width: '15%' }}>Nombre</div>
                 <div style={{ width: '10%' }}>Cupo Solicitado</div>
-                <div style={{ width: '8%' }}>Franquicia</div>
+                <div style={{ width: '12%' }}>Franquicia</div>
                 <div style={{ width: '8%' }}>Tasa</div>
                 <div style={{ width: '20%' }}>Fecha de Venta</div>
                 <div style={{ width: '20%' }}>Fecha de Modificación</div>
@@ -35,7 +35,7 @@ export const SeguiProduct = () => {
                             <div style={{ width: '5%' }}>{ele.id}</div>
                             <div style={{ width: '15.5%' }}>{ele.producto}</div>
                             <div style={{ width: '10%' }}>{ele.cupo}</div>
-                            <div style={{ width: '8%' }}>{ele.franquicia}</div>
+                            <div style={{ width: '12%' }}>{ele.franquicia}</div>
                             <div style={{ width: '8%' }}>{ele.tasa}</div>
                             <div style={{ width: '20%' }}>{new Date(ele.createdAt).toLocaleString()}</div>
                             <div style={{ width: '20%' }}>{new Date(ele.updatedAt).toLocaleString()}</div>
